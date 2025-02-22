d990755c6b806e1fb3c029d3ea5bc30fb953d40d
Gor Martsen <Gor.martsen@gmail.com>
2025-02-22 16:47:14 -0500

    codestyle

--- src/debug-browser.js
+++ src/debug-browser.js
@@ -1,12 +1,10 @@
-
 const debug = {
-  debug: function() {
-    if(window.isDebug) {
+  debug: function () {
+    if (window.isDebug) {
       console.info.apply(console, arguments);
     }
   },
   log: window.console.log.bind(window.console),
-}
+};
 
-
-export default debug
+export default debug;
